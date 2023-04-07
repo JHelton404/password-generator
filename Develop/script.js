c43e3760662e3c187ab1c1c7fc870a40c3811fbf
Jhelt <jonhelton405@gmail.com>
2023-04-07 00:00:02 -0400

    New functional draft

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characters =[];
+var password = document.getElementById("password")
 
 // Created arrays for letters, capital letters, numbers, and symbols
-var NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var CAPITALS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var SYMBOLS = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ',', '.', '?']
 
@@ -11,24 +13,30 @@ var SYMBOLS = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', 
 // Solution, create prompts for each user input when button is clicked.
 
 // Write password to the #password input
-function writePassword(length, inclNum, inclCap, inclSym) {
-  let characters = LETTERS
-  var length = (prompt("How many characters will your password be? Enter a value between 1-50 (tip: use the highest allowed character limit for maximum protection)"))
-  var inclNum = prompt("Would you like to include numbers? yes/no")
-  if (inclNum === "yes") characters = characters.concat(NUMBERS)
-  var inclCap = prompt("Would you like to include Capital Letters? yes/no")
-  if (inclCap === "yes") characters = characters.concat(CAPITALS)
-  var inclSym = prompt("Would you like to include symbols? yes/no")
-  if (inclSym === "yes") characters = characters.concat(SYMBOLS)
+function writePassword(length, inclLet, inclNum, inclCap, inclSym) {
+  characters = [];
+  var length = (prompt("How many characters will your password be? Enter a value between 8-128 \n(tip: use the highest allowed character limit for maximum protection)"))
+  var inclLet = confirm("Would you like to include Lowercase Letters? \npress okay for yes")
+  if (inclLet === true) characters = characters.concat(LETTERS)
+  var inclNum = confirm("Would you like to include numbers? \npress okay for yes")
+  if (inclNum === true) characters = characters.concat(NUMBERS)
+  var inclCap = confirm("Would you like to include Capital Letters? \npress okay for yes")
+  if (inclCap === true) characters = characters.concat(CAPITALS)
+  var inclSym = confirm("Would you like to include symbols? \npress okay for yes")
+  if (inclSym === true) characters = characters.concat(SYMBOLS)
+  if (inclLet != true && inclCap != true && inclNum != true && inclSym != true) alert("You must select some type of character")
 
-  let passwordCharacters = ""
+  let possibleCharacters = []
+  if (length >= 8 && length <= 128)
   for (let i = 0; i < length; i++) {
     // get a random # between 0 and the # of allowed characters
-    const random = Math.floor(Math.random() * ( ( characters.lenght-1) - 0 + 1) + 0)
-    passwordCharacters += characters[random]
+    const random = Math.floor(Math.random() * characters.length)
+    possibleCharacters += (characters[random])
+  } else {
+    alert("Password must be between 8 and 128 characters")
   }
-  console.log(passwordCharacters)
-  return passwordCharacters
+  console.log(possibleCharacters)
+  password.innerText = possibleCharacters
 }
 
 // Add event listener to generate button
